280ca097fd938adffc24103eb35706f3f1ebf8ed
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-06 23:07:05 +0700

    Update index.js
    feature/userController + authentication with WJT

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const Api = require("./api.router");
+const Auth = require("./auth.router");
+
 const route = (app) => {
   app.use("/api", Api);
+  app.use("/auth", Auth);
 };
 
 module.exports = route;
