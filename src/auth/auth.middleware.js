449ac7e1a899eef1678149410c2a8ef5bf221fef
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-06 23:06:57 +0700

    Create auth.middleware.js

---
+++ src/auth/auth.middleware.js
@@ -0,0 +1,28 @@
+const authMethod = require("./auth.methods");
+const connect = require("../config/configDB");
+
+exports.isAuth = async (req, res, next) => {
+  // Lấy access token từ header
+  const accessTokenFromHeader = req.headers.x_authorization;
+  if (!accessTokenFromHeader) {
+    return res.status(401).send("access token not found!");
+  }
+
+  const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
+
+  const verified = await authMethod.verifyToken(
+    accessTokenFromHeader,
+    accessTokenSecret
+  );
+  if (!verified) {
+    return res.status(401).send("You do not have access to this feature!");
+  }
+  const query = "SELECT * FROM user WHERE phone = ?";
+  try {
+    let [data] = connect.execute(query, [verified.payload.phone]);
+    req.user = data[0];
+  } catch (error) {
+    console.log("auth error");
+  }
+  return next();
+};
