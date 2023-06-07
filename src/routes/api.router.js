8d3e9841a9c10ca10f8dfab02d6883bb62f02e23
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-07 13:05:19 +0700

    Update api.router.js
    feature/authentication-jwt

--- src/routes/api.router.js
+++ src/routes/api.router.js
@@ -7,6 +7,7 @@ const upload = require("../middlewares/upload");
 //product
 Router.post(
   "/products/addProduct",
+  auth.isAuth,
   upload.single("imageProduct"),
   productController.addProduct
 );
@@ -15,12 +16,20 @@ Router.get(
   auth.isAuth,
   productController.getBestSelling
 );
-Router.get("/products/newArrivals", productController.getNewArrivals);
-Router.get("/products/:category", productController.getProductByCategory);
+Router.get(
+  "/products/newArrivals",
+  auth.isAuth,
+  productController.getNewArrivals
+);
+Router.get(
+  "/products/:category",
+  auth.isAuth,
+  productController.getProductByCategory
+);
 
 //user
-Router.get("/user/:id", userController.getAccount);
-Router.put("/user/:id", userController.updateAccount);
+Router.get("/user/:id", auth.isAuth, userController.getAccount);
+Router.put("/user/:id", auth.isAuth, userController.updateAccount);
 Router.post("/user/createAccount", userController.createAccount);
 Router.post("/user/login", userController.login);
 
