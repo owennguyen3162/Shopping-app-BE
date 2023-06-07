02d645a046ceedef5d35c6a90cddadb71c55d2c2
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-08 00:43:50 +0700

    Update api.router.js
    feature/cartController

--- src/routes/api.router.js
+++ src/routes/api.router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const productController = require("../controllers/productController");
 const userController = require("../controllers/userController");
+const cartController = require("../controllers/cartController");
+
 const auth = require("../auth/auth.middleware");
 const Router = express.Router();
 const upload = require("../middlewares/upload");
@@ -32,5 +34,15 @@ Router.get("/user/:id", auth.isAuth, userController.getAccount);
 Router.put("/user/:id", auth.isAuth, userController.updateAccount);
 Router.post("/user/createAccount", userController.createAccount);
 Router.post("/user/login", userController.login);
+
+//cart 
+Router.post("/cart/addToCart", auth.isAuth, cartController.addToCart);
+Router.post("/cart/checkout/:userId", auth.isAuth, cartController.checkout);
+
+Router.delete("/cart/removeToCart/:id", auth.isAuth, cartController.removeToCart);
+Router.get("/cart/:id",  auth.isAuth,cartController.getCartById);
+
+
+
 
 module.exports = Router;
