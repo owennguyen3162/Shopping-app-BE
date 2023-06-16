b53ffe472aecfa7ad291ecc5cde478741156dccd
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-16 12:27:55 +0700

    Update viewEngine.js

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const viewEngine = (app) => {
   app.use(express.static("./uploads"));
+  app.use(express.static("./src/public/assets"));
+  app.set('views', './src/views')
+  app.set('view engine', 'ejs')
 };
 
 module.exports = viewEngine;
