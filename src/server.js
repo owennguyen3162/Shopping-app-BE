d7b869c3dd73d9ad413eb43e3084bfe5c3cccdc7
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-17 21:32:52 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,14 +6,15 @@ const route = require("./routes");
 const fs = require("fs");
 const dir = "./uploads";
 const viewEngine = require("./config/viewEngine")
+const methodOverride = require('method-override')
 if (!fs.existsSync(dir)) {
   fs.mkdirSync(dir);
 }
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 viewEngine(app);
 route(app);
-
 app.listen(PORT, () => {
   console.log("LISTENING TO PORT " + PORT);
 });
