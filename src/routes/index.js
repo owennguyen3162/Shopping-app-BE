4617d5b20bb358fbdb8d9394003abf324f817b7d
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-17 21:32:48 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,15 @@
 const Api = require("./api.router");
 const Auth = require("./auth.router");
 const Home = require("./home.router");
+const User = require("./user.router");
+const Order = require("./order.router");
 
 const route = (app) => {
   app.use("/api", Api);
   app.use("/auth", Auth);
   app.use("/home", Home);
+  app.use("/user", User);
+  app.use("/order", Order);
 
 };
 
