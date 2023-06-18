6087093243fc07d482086438fef386383a1afc20
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-19 00:16:24 +0700

    Update home.router.js

--- src/routes/home.router.js
+++ src/routes/home.router.js
@@ -7,7 +7,7 @@ Route.post(
   upload.single("imageProduct"),
   HomeController.addProduct
 );
-Route.delete("/deleteProduct/:productId");
+Route.delete("/deleteProduct/:productId",HomeController.deleteProduct);
 Route.put("/editProduct/:productId", upload.single("imageProduct"),HomeController.editProduct);
 Route.get("/addProduct", HomeController.viewAddProduct);
 Route.get("/editProduct/:productId", HomeController.viewEditProduct);
