a150663c468d44f45626ba1b6817d894cc51b35c
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-19 00:16:13 +0700

    Update viewEngine.js

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -3,6 +3,7 @@ const viewEngine = (app) => {
   app.use(express.static("./uploads"));
   app.use(express.static("./src/public/assets"));
   app.use('/home',express.static("./src/public/assets"));
+  app.use('/order',express.static("./src/public/assets"));
   app.set('views', './src/views')
   app.set('view engine', 'ejs')
 };
