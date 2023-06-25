42938e4d841ddb9203e3eba4e47177ea4a74e26f
owennguyen3162 <nguyentuan3162@gmail.com>
2023-06-25 17:33:55 +0700

    Update user.router.js

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -3,5 +3,7 @@ const UserController = require("../controllers/userController");
 const Route = require("express").Router();
 
 Route.get("/", UserController.getUSers);
+Route.get("/profile", UserController.profile);
+
 
 module.exports = Route;
