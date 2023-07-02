4a29575972b7529b61b424f723d5ddf3aebe375b
owennguyen3162 <nguyentuan3162@gmail.com>
2023-07-02 23:19:57 +0700

    Update auth.router.js

--- src/routes/auth.router.js
+++ src/routes/auth.router.js
@@ -1,6 +1,14 @@
 const Router = require("express").Router();
 const authController = require("../auth/auth.controller")
-
-Router.post("/refreshToken", authController);
+const auth = require("../auth/auth.middleware");
+Router.use((req, res, next) => {
+  const token = req.session.User;
+  console.log(token);
+  if (token) {
+    req.headers.x_authorization = token.token;
+  }
+  next();
+});
+Router.post("/refreshToken",auth.isAuth, authController);
 
 module.exports = Router;
