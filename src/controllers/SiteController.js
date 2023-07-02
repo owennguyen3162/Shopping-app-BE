19aedd53f9a48c79dcc6d9c5d4dfc7cafff4ef08
owennguyen3162 <nguyentuan3162@gmail.com>
2023-07-02 23:19:55 +0700

    Update SiteController.js

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -1,8 +1,41 @@
+const connect = require("../config/configDB");
+const authMethod = require("../auth/auth.methods");
+const session = require("express-session");
 const login = (req, res) => {
   res.render("Login");
 };
-const HandleLogin = (req, res) => {
-  res.redirect("/home");
+const HandleLogin = async (req, res) => {
+  const { username, password } = await req.body;
+  if (!username || !password) {
+    return res.json({ msg: "login fail " });
+  }
+
+  const query = "SELECT * FROM user WHERE name = ? and role = 'admin'";
+  try {
+    const [data] = await connect.execute(query, [username]);
+    if (data.length > 0) {
+      if (data[0].password === password) {
+        const payload = { username: data[0].name, role: data[0].role };
+        const token = await authMethod.generateToken(
+          payload,
+          process.env.ACCESS_TOKEN_SECRET,
+          process.env.ACCESS_TOKEN_LIFE_WEB
+        );
+        if (!token) {
+          return res.json({ msg: "Error in generate token " });
+        }
+        req.session.User = {
+          token: token,
+        };
+        return res.redirect("/home");
+      }
+      return res.json({ msg: "login fail !!" });
+    } else {
+      res.json({ msg: "Not found userName !!" });
+    }
+  } catch (error) {
+    res.json({ msg: "login fail " + error });
+  }
 };
 
 module.exports = { login, HandleLogin };
