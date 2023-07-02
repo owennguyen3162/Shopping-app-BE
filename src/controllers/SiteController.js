bd82cf0adca6d51fe3597c1ae537ac00d1c26ce1
owennguyen3162 <nguyentuan3162@gmail.com>
2023-07-02 23:38:52 +0700

    Update SiteController.js

--- src/controllers/SiteController.js
+++ src/controllers/SiteController.js
@@ -5,17 +5,17 @@ const login = (req, res) => {
   res.render("Login");
 };
 const HandleLogin = async (req, res) => {
-  const { username, password } = await req.body;
-  if (!username || !password) {
+  const { phone, password } = await req.body;
+  if (!phone || !password) {
     return res.json({ msg: "login fail " });
   }
 
-  const query = "SELECT * FROM user WHERE name = ? and role = 'admin'";
+  const query = "SELECT * FROM user WHERE phone = ? and role = 'admin'";
   try {
-    const [data] = await connect.execute(query, [username]);
+    const [data] = await connect.execute(query, [phone]);
     if (data.length > 0) {
       if (data[0].password === password) {
-        const payload = { username: data[0].name, role: data[0].role };
+        const payload = { phone: data[0].phone, role: data[0].role };
         const token = await authMethod.generateToken(
           payload,
           process.env.ACCESS_TOKEN_SECRET,
