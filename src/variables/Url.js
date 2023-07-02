62a5b1cabbc3daa5654d7aab7253b2faa36bd9c0
owennguyen3162 <nguyentuan3162@gmail.com>
2023-07-02 23:20:07 +0700

    Update Url.js

--- src/variables/Url.js
+++ src/variables/Url.js
@@ -1,5 +1,5 @@
-const URL_FOR_FPL = "http://10.24.55.219:3000";
-const URL_FOR_HOME = "http://10.24.55.219:3000";
+const URL_FOR_FPL = "http://192.168.0.103:3000";
+const URL_FOR_HOME = "http://192.168.0.103:3000";
 
 module.exports = {
     URL_FOR_FPL,
