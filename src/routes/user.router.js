cc37e86b4d9fb1bf1231ea19c19bac753377d595
owennguyen3162 <nguyentuan3162@gmail.com>
2023-07-02 23:20:04 +0700

    Update user.router.js

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,9 +1,16 @@
 const UserController = require("../controllers/userController");
 
 const Route = require("express").Router();
-
-Route.get("/", UserController.getUSers);
-Route.get("/profile", UserController.profile);
-
+const auth = require("../auth/auth.middleware");
+Route.use((req, res, next) => {
+  const token = req.session.User;
+  console.log(token);
+  if (token) {
+    req.headers.x_authorization = token.token;
+  }
+  next();
+});
+Route.get("/", auth.isAuth, UserController.getUSers);
+Route.get("/profile", auth.isAuth, UserController.profile);
 
 module.exports = Route;
